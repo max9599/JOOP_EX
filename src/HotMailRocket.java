0e43bbb6b4d568d69d2c94d0f8de01ec3674f834
Maxim Gromov <maxim.gromov@heisi.eu>
2016-01-07 12:52:54 +0200

    hot resist rocket pack method override

--- src/HotMailRocket.java
+++ src/HotMailRocket.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
+
 /**
  * Created by maxim on 7.01.16.
  */
 public class HotMailRocket extends MailRocket {
 
     public static final int ONE_HOT_START_FUEL = 25;
+    public static final int MAX_PACKAGES_WEIGHT = 80;
 
     @Override
     public void start() {
@@ -14,4 +17,16 @@ public class HotMailRocket extends MailRocket {
         fuel -= ONE_HOT_START_FUEL;
     }
 
+    @Override
+    public void pack(Package packingPackage) {
+        if (packages == null) {
+            packages = new ArrayList<>();
+        }
+
+        if (totalPackagesWeight + packingPackage.getPackageWeight() > MAX_PACKAGES_WEIGHT) {
+            System.out.println("You can't pack so much packages on one hot resist rocket. Max 80");
+        } else {
+            packages.add(packingPackage);
+        }
+    }
 }
